dbebe02d4b1ca7a5c7fadc8aa69794040d5a74cd
CS-Evan <112963716+CS-Evan@users.noreply.github.com>
2022-12-01 00:29:35 -0500

    Added nav bar; modified index page and css (#7)
    * Added navbar, and modified index text
    
    * Changes to jumbotron layout, index text, and added nav-bar

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/contactlist', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/test', function(req, res, next) {
+  res.render('test', { title: 'Test' });
+});
+
 module.exports = router;
